6a121ee77ec787950fa1df30db05c069235083dd
Tam Pham <tampham.dev@gmail.com>
2021-07-10 13:43:49 +0700

    Add styled

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+interface Props {
+  count: number,
+  callback: any
+}
+
+const Button = ({count ,callback}:Props) => (
+    <button onClick={callback}>Count: {count}</button>
+  )
+
+
+export default Button
